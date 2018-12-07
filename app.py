fb6edd76cd5782b50ce0e3aa1733f3dd70cf5f06
Karan Sharma <karans@GGT.local>
2018-12-07 10:12:56 -0500

    Stable added secrets secretly

--- app.py
+++ app.py
@@ -2,10 +2,34 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/")
 
+# The route() decorator to binds a function to a URL.
+@app.route("/")
 def hello():
     return "WELCOME TO IMRPID"
+
+# The route() decorator to binds a function to a URL.
+@app.route("/register")
+def register():
+    return "IMPLEMENT REGISTER API"
+
+
+# The route() decorator to binds a function to a URL.
+@app.route("/login")
+def login():
+    return "IMPLEMENT LOGIN API"
+
+# The route() decorator to binds a function to a URL.
+@app.route("/upload")
+def upload():
+    return "IMPLEMENT UPLOAD API"
+
+
+# The route() decorator to binds a function to a URL.
+@app.route("/alluploads")
+def alluploads():
+    return "IMPLEMENT ALLUPLOADS API"
+
 
 
 if __name__ == '__main__':
