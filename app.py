f3d62e9faaefa55cad43dea95607abc7e38258c1
Karan Sharma <karans@GGT.local>
2018-12-07 12:43:14 -0500

    Stable branch completed first upload to s3

--- app.py
+++ app.py
@@ -1,4 +1,9 @@
 from flask import Flask
+import config
+import boto3
+from botocore.exceptions import ClientError
+import time
+import calendar
 
 app = Flask(__name__)
 
@@ -22,6 +27,17 @@ def login():
 # The route() decorator to binds a function to a URL.
 @app.route("/upload")
 def upload():
+    #file_key = datetime.datetime.now() + '_sample_stl.stl'
+    file_key = str(calendar.timegm(time.gmtime())) + '_sample_stl.stl'
+    try:
+        print "DEBUG HERE"
+        s3_client = boto3.client('s3', aws_access_key_id=config.aws_access_key_id, aws_secret_access_key=config.aws_secret_access_key)
+        print "CLIENT CREATED SUCESSFULLY"
+        print s3_client
+    except ClientError as ce:
+        cerror_message = ce.response['Error']['Message']
+        print "ERROR: "+cerror_message
+    s3_client.upload_file('sample_stl.stl',config.bucket_name,file_key)
     return "IMPLEMENT UPLOAD API"
 
 
